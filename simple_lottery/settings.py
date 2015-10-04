7072eade9565c9f8654784bf192ee6a1e59ee0e5
Sascha Peter <sascha.peter@stickyboard.co.uk>
2015-10-04 16:49:51 +0100

    new: dev: Add localflavor to project

--- simple_lottery/settings.py
+++ simple_lottery/settings.py
@@ -26,6 +26,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = (
+
+    'localflavor',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
