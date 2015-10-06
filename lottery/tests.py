4f9163cb9490c4ec732f4f0c382f71364e653afe
Sascha Peter <sascha.peter@stickyboard.co.uk>
2015-10-06 21:32:34 +0100

    fix: dev: Adjust docstrings and bump version

--- lottery/tests.py
+++ lottery/tests.py
@@ -1,7 +1,7 @@
 """This file contains all lottery related test cases
 
 @author: Sascha Peter <sascha.o.peter@gmail.com>
-@version: 0.3.0
+@version: 0.4.0
 @since: 2015-10-06
 """
 from django.test import Client, TestCase, RequestFactory
