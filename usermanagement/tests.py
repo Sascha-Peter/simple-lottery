a285bdfef21054cdef2c541a40f11c14780836e2
Sascha Peter <sascha.o.peter@gmail.com>
2017-06-10 22:19:55 +0100

    fix: Adjust tests not to use hardcoded password

--- usermanagement/tests.py
+++ usermanagement/tests.py
@@ -1,17 +1,26 @@
 """Unit and functional tests for the user management."""
 from django.test import TestCase, Client, RequestFactory
 from django.contrib.auth.models import User, AnonymousUser
+import random
+import string
 
 
 class UserTest(TestCase):
     """Unit tests for testing user related actions."""
+
+    user_password = ''.join(
+        random.SystemRandom().choice(
+            string.ascii_uppercase + string.digits
+        ) for _ in range(16)
+    )
 
     def setUp(self):
         """Define test users and factories."""
         self.client = Client()
         self.factory = RequestFactory()
         self.user_one = User.objects.create_user(
-            username="dummy", email="dummy@test.com", password="dummypw")
+            username="dummy", email="dummy@test.com",
+            password=self.user_password)
         self.user_anonymous = AnonymousUser()
 
     def test_homepage(self):
@@ -21,5 +30,6 @@ class UserTest(TestCase):
 
     def test_login(self):
         """Test if the login process is working for a defined user."""
-        test_user = self.client.login(username='dummy', password='dummypw')
+        test_user = self.client.login(username='dummy',
+                                      password=self.user_one.password)
         self.assertEqual(test_user, True)
